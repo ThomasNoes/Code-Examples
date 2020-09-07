aa99f60dc73784fea09d40742df0775ae6d915f1
ThomasNoes <70569890+ThomasNoes@users.noreply.github.com>
2020-09-07 15:11:37 +0200

    example for the SOLID Principles in action
    I made a BallController and two different types of movement classes that uses the same interfaces

---
+++ example scripts/SOLID Ballcontroller/BallController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    //This script is for handling the input for moving a character around  
+    //It translates the input into actions for any object that implements the IControllable interface
+    //The character does not need to be a ball but for that is just what I chose for this example, hence the name BallController
+   
+    private IControllable character;
+
+
+    private void Awake()
+    {
+        character = GetComponent<IControllable>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (character is IMovable) // if the character implements the Imovable interface it can move using the axes called Vertical and Horizontal
+        {
+            IMovable moveable = character as IMovable;
+            moveable.MoveForward(Input.GetAxis("Vertical"));
+            moveable.MoveRight(Input.GetAxis("Horizontal"));
+        }
+        if (character is IJumpable) // if the character implements the Ijumpable interface, it can jump
+        {
+            IJumpable jumpable = character as IJumpable;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                jumpable.Jump();
+            }
+        }
+    }
+}
+
+
